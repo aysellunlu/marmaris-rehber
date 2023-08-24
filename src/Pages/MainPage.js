b3542e83bb843764a464de4e87f703fa5f333f93
Aysel Ünlü <aysell.unlu48@gmail.com>
2023-08-24 21:37:40 +0300

    modal geçici headers

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,63 +1,96 @@
 import React, { useEffect, useState } from "react";
 import "../Styles/MainPage.css";
 import Modal from "react-bootstrap/Modal";
-import BigChef from "../img/bigchefs.png";
 import selectOptions from "../Datas/SelectOption";
 import Datas from "../Datas/data.json";
 import WelcomeData from "../Datas/welcome.json";
-import axios from "axios";
+import SearchIcon from "../Icons/SearchIcon";
 
-function MyVerticallyCenteredModal(props) {
-  const [items, setItems] = useState(Datas);
-  const [data, setData] = useState([]);
+const MainPage = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [selectedValue, setSelectedValue] = useState("");
+  const handleSelectChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+  const selectedOption = selectOptions.find(
+    (option) => option.value === selectedValue
+  );
+  function MyVerticallyCenteredModal(props) {
+    const [items, setItems] = useState(Datas);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios.get('/api/data')
-      .then(response => setData(response.data))
-      .catch(error => console.error(error));
-  }, []);
 
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          <h4>Restaurants & Cafes</h4>
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {items &&
-          items.map((x) => (
-            <div className="modal-cards pb-4">
-              <div className="modal-card-header mt-4 mb-4">{x.name}</div>
-              <div className="row">
-                <div className="col">
-                  <img src={x.imageURL} width="100%" />
-                </div>
-                <div className="col">
-                  <div className="modal-card-text mb-2">
-                    <b>Service: </b> {x.servicesType}
+    //BURASI ÇIKTI GÖRMEK İÇİN, GEÇİCİ !!!!!! DÜZENLEEEE
+
+    const selectedHeader = () => {
+      if (selectedValue == 1) {
+        return <p>{selectOptions[0].label}</p>;
+      } else if (selectedValue == 2) {
+        return <p>{selectOptions[1].label}</p>;
+      } else if (selectedValue == 3) {
+        return <p>{selectOptions[2].label}</p>;
+      } else if (selectedValue == 4) {
+        return <p>{selectOptions[3].label}</p>;
+      } else if (selectedValue == 5) {
+        return <p>{selectOptions[4].label}</p>;
+      } else if (selectedValue == 6) {
+        return <p>{selectOptions[4].label}</p>;
+      } else if (selectedValue == 7) {
+        return <p>{selectOptions[6].label}</p>;
+      } else if (selectedValue == 8) {
+        return <p>{selectOptions[7].label}</p>;
+      } else if (selectedValue == 9) {
+        return <p>{selectOptions[8].label}</p>;
+      } else if (selectedValue == 10) {
+        return <p>{selectOptions[9].label}</p>;
+      } else if (selectedValue == 11) {
+        return <p>{selectOptions[10].label}</p>;
+      } else if (selectedValue == 12) {
+        return <p>{selectOptions[11].label}</p>;
+      }
+    };
+
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            <h4> {selectedHeader()}</h4>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {items &&
+            items.map((x) => (
+              <div className="modal-cards pb-4" >
+                <div className="modal-card-header mt-4 mb-4">{x.name}</div>
+                <div className="row">
+                  <div className="col">
+                    <img src={x.imageURL} width="100%" alt={x.name} />
                   </div>
-                  <div className="modal-card-text mb-2">
-                    <b>Address: </b> {x.address}
-                  </div>
-                  <div className="modal-card-text mb-2">
-                    <b>Working Hours: </b> {x.workingHours}
+                  <div className="col">
+                    <div className="modal-card-text mb-2">
+                      <b>Service: </b> {x.servicesType}
+                    </div>
+                    <div className="modal-card-text mb-2">
+                      <b>Address: </b> {x.address}
+                    </div>
+                    <div className="modal-card-text mb-2">
+                      <b>Working Hours: </b> {x.workingHours}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-      </Modal.Body>
-    </Modal>
-  );
-}
-const MainPage = () => {
-  const [modalShow, setModalShow] = React.useState(false);
+            ))}
+        </Modal.Body>
+      </Modal>
+    );
+  }
+
+  console.log("selected value :", selectedValue);
   return (
     <div className=" main-content">
       <div className="main-bg-img">
@@ -66,7 +99,9 @@ const MainPage = () => {
           class="input-field"
           placeholder="What do you want to search ?"
         />
-        <button className="search-btn">ARA</button>
+        <button className="search-btn">
+          <SearchIcon />
+        </button>
       </div>
       <div className="marmaris-center-container container-fluid">
         <div className="marmaris-center-header p-3 mt-4">
@@ -79,17 +114,21 @@ const MainPage = () => {
           <select
             class="form-select col-lg col-sm-12 mx-2 mt-3"
             aria-label="Default select example"
+            value={selectedValue}
+            onChange={handleSelectChange}
           >
             {selectOptions &&
               selectOptions.map((opt) => (
-                <option value={opt.value}>{opt.label}</option>
+                <option key={opt.value} value={opt.value}>
+                  {opt.label}
+                </option>
               ))}
           </select>
           <button
             className="list-btn mt-3 py-2 col-lg-3 col-sm-12 mx-2"
             onClick={() => setModalShow(true)}
           >
-            List
+            LIST
           </button>
         </div>
         <MyVerticallyCenteredModal
